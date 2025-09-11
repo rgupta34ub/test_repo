d71d0750ea61adb278bdb07967525ed354335a7e
rgupta34ub <rgupta34@buffalo.edu>
2025-09-11 09:44:26 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print("Sample")
+# helloworld.py
+import os
+import sys
+
+EXAMPLE_KEY = os.environ.get("EXAMPLE_KEY")
+if not EXAMPLE_KEY:
+    print("ERROR: EXAMPLE_KEY environment variable is not set.", file=sys.stderr)
+    sys.exit(1)
+
+print("Hello World! Secret length:", len(EXAMPLE_KEY))
+# ...use EXAMPLE_KEY safely in your code...
